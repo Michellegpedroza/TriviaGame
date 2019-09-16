68951665139ff331821e92bbb98d36dbb2cb60a4
Michelle Gonzalez <m.chellegpedroza@gmail.com>
2019-09-16 14:21:34 -0700

    added the trivia questions to my JS file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,33 @@
-const questions = []
+const questions = [ {
+  question: `In which video does Michael Jackson play a Zombie?`
+  choices: ['Bad', `Beat It`, `Thriller`, `PYT`]
+  answer:`2`
+},
+{
+  question: `Which of these is NOT a Whitney Houston song?`
+  choices: [`I Wanna Dance With Somebody`, `Greatest Love of All`, `Time After Time`, `I Will Always Love You`]
+  answer: `2`
+},
+  {
+  question: `What Comes Before "...Baby One More Time?"`
+  choices: [`Kiss Me`, `Hold Me`, `Hug Me`, `Hit Me`]
+  answer: `3`
+  },
+  {
+  question: `Complete the Mark Ronson Song title: Uptown...`
+  choices: [`Funk`, `Beat`, `Girl`, `Tunes`]
+  answer: `0`
+  },
+  {
+  question: `What is the song that launched Justin Bieber to stardom?`
+  choices: [`What Do You Mean?`, `Baby`, `Sorry`, `Love Yourself`]
+  answer: `1`
+  }
+]
+
 let score = 0
+
+for(let i=0; i < questions.length; i++){
+  let question = document.getElementById(`myQuestion`) = questions[i]
+    if ( )
+  }
