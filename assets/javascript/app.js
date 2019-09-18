08e588b7e4378b00a16000a70b25f766e350e426
Michelle Gonzalez <m.chellegpedroza@gmail.com>
2019-09-18 13:08:04 -0700

    had technical difficulties pushing new code. Loop was not working, created functionality to first question only, when wrong answer is click, the button turns red and when correct answer is click button turns green

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+// Questions
 const questions = [{
   question: `In which video does Michael Jackson play a Zombie?`,
   choices: ['Bad', `Beat It`, `Thriller`, `PYT`],
@@ -25,33 +26,92 @@ const questions = [{
 }
 ]
 
+// Variables
+//Question & Choices variables
+let question = document.getElementById(`myQuestion`)
+let choice1 = document.getElementById(`choice1`)
+let choice2 = document.getElementById(`choice2`)
+let choice3 = document.getElementById(`choice3`)
+let choice4 = document.getElementById(`choice4`)
+
+// Score at the end of the game variables
 let correctA = 0
 let wrongA = 0
 
+//Timer variable
 let time = 15
 
-let seconds = setInterval(function () {
-  document.getElementById(`myTimer`).innerHTML = time + ` seconds remaining`
-  time -= 1;
-  if (time <= 0) {
-    clearInterval(seconds);
-    document.getElementById(`myTimer`).innerHTML = `Times Up!`
-  }
-}, 1000);
-
-const ask = () => {
-  for (let i = 0; i < questions.length; i++) {
-    let question = document.getElementById(`myQuestion`).innerHTML = questions[i].question
-    let choice1 = document.getElementById(`choice1`).innerHTML = questions[i].choices[0]
-    let choice2 = document.getElementById(`choice2`).innerHTML = questions[i].choices[1]
-    let choice3 = document.getElementById(`choice3`).innerHTML = questions[i].choices[2]
-    let choice4 = document.getElementById(`choice4`).innerHTML = questions[i].choices[3]
-
-
-  }
+let timer = () => {
+  // Countdown Timer
+  const seconds = setInterval(function () {
+    document.getElementById(`myTimer`).innerHTML = time + ` seconds remaining`
+    time -= 1;
+    if (time <= 0) {
+      clearInterval(seconds);
+      document.getElementById(`myTimer`).innerHTML = `Times Up!`
+    }
+  }, 1000);
 }
-ask()
 
+timer()
+//First Question
+let myQuestion = question.innerHTML = questions[0].question
+//Choices
+choice1.innerHTML = questions[0].choices[0]
+choice2.innerHTML = questions[0].choices[1]
+choice3.innerHTML = questions[0].choices[2]
+choice4.innerHTML = questions[0].choices[3]
 
+//Answer to question
+let answer = questions[0].answer
 
+//If correct button is green, if wrong button is red
+let choices = document.querySelectorAll(`.choices`)
+for (let x = 0; x < choices.length; x++) {
+  choices[x].addEventListener(`click`, () => {
+    if (choices[x].value === answer) {
+      choices[x].style.backgroundColor = `green`
+      choices[x].innerHTML = `Correct!!!`
 
+      correctA++
+
+    } else {
+      choices[x].style.backgroundColor = `red`
+      choices[x].innerHTML = `WRONG!`
+
+      wrongA++
+    }
+
+  })
+}
+
+//
+// const ask = () => {
+//   // Looping through the questions
+//   //WOULD IT BE BEST TO USE THE FOREACH METHOD?//
+//   for (let i = 0; i < questions.length; i++) {
+//     let myQuestion = question.innerHTML = questions[i].question
+
+//     choice1.innerHTML = questions[i].choices[0]
+//     choice2.innerHTML = questions[i].choices[1]
+//     choice3.innerHTML = questions[i].choices[2]
+//     choice4.innerHTML = questions[i].choices[3]
+
+//     let answer = questions[i].answer
+//     console.log(myQuestion)
+//     console.log(answer)
+
+//     let choices = document.querySelectorAll(`.choices`)
+//     //Looping through buttons
+//     for (let x = 0; x < choices.length; x++) {
+//       choices[x].addEventListener(`click`, () => {
+
+//       })
+//     }
+
+//   }
+// }
+
+// questions.forEach(ask){
+
+// }
